2133915212c99088c8605e19cb8a3d6a4c6d024b
yugo333 <yugo199103@gmail.com>
2019-12-06 16:48:38 +0900

    group_func

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
-  resources :users, only: [:edit, :update]
+  root "groups#index"
+  resources :users, only: [:edit, :update] 
+  #  :index
+  resources :groups, only: [:new, :create, :edit, :update]do
+    resources :message, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
