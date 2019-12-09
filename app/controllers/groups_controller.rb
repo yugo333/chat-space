18d985c1eaef802951db3be7d3a88edbd66aa548
yugo333 <yugo199103@gmail.com>
2019-12-09 16:49:10 +0900

    messages_func

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  
+
   before_action :set_group, only: [:edit, :update]
 
   def index
@@ -19,9 +19,12 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to  group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users
   root "groups#index"
-  resources :users, only: [:edit, :update] 
-  #  :index
-  resources :groups, only: [:new, :create, :edit, :update]do
-    resources :message, only: [:index]
+  resources :users, only: [:index,:edit, :update] 
+  resources :groups, only: [:index,:new, :create, :edit, :update]do
+    resources :messages, only: [:index, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
