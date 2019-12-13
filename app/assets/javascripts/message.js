c9b9bc467cf13e3ff1bfb29910e83893cbba6caf
yugo333 <yugo199103@gmail.com>
2019-12-13 19:04:46 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,28 @@
-$(function(){
-  function buildHTML(message) {
-    if (message.image) {
-      img = `<img class="lower-message__image" src="${message.image}">`
-    }else{
-      var img = ""
-    }
-    var html = `<div class="message" data-message-id="${message.user_id}">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                      ${ message.name }
-                    </div>
-                    <div class="upper-message__date">
-                      ${ message.created_at }
-                    </div>
-                  </div>
-                  <div class="lower-message">
-                    <p class="lower-message__content">
-                    ${ message.content }
-                    ${ img }
-                    </p>
-                  </div>
-              </div>`
+$(function() {
+  var buildHTML = function(message) {
+    image=(message.image) ? `<img src=${message.image} class="lower-message__image" >` :"";
+    var html = `
+    <div class="message" data-id= ${message.id} >
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name} 
+        </div>
+        <div class="upper-message__date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="lower-message">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+          ${image}
+      </div>
+    </div>
+    `
     return html;
-  }
+  };
+
+
 
   $("#new_message").on("submit", function(e){
     e.preventDefault()
@@ -50,4 +50,65 @@ $(function(){
       $(".submit").prop("disabled",false);
     })
   })
+  
+
+
+
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $(".message:last").data("id");
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i,message) {
+          console.log(message)
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $(".main__chat_message").append(insertHTML);
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+  setInterval(reloadMessages, 7000);
 });
+
+
+
+
+// $(function(){
+//   function buildHTML(message) {
+//     if (message.image) {
+//       img = `<img class="lower-message__image" src="${message.image}">`
+//     }else{
+//       var img = ""
+//     }
+//     var html = `<div class="message" data-message-id="${message.user_id}">
+//                   <div class="upper-message">
+//                     <div class="upper-message__user-name">
+//                       ${ message.name }
+//                     </div>
+//                     <div class="upper-message__date">
+//                       ${ message.created_at }
+//                     </div>
+//                   </div>
+//                   <div class="lower-message">
+//                     <p class="lower-message__content">
+//                     ${ message.content }
+//                     ${ img }
+//                     </p>
+//                   </div>
+//               </div>`
+//     return html;
+//   }
