530f531ec8e930d4bc0a71d174a6285d48f2fdeb
yugo333 <yugo199103@gmail.com>
2019-12-17 16:30:26 +0900

    s追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -55,32 +55,32 @@ $(function() {
 
 
     var reloadMessages = function() {
-      if (window.location.href.match(/\/groups\/\d+\/message/)){
-      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      last_message_id = $(".message:last").data("id");
-      $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-        url: "api/messages",
-        //ルーティングで設定した通りhttpメソッドをgetに指定
-        type: 'get',
-        dataType: 'json',
-        //dataオプションでリクエストに値を含める
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        //追加するHTMLの入れ物を作る
-        var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(messages, function(i,message) {
-          insertHTML += buildHTML(message)
+        if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+        last_message_id = $(".message:last").data("id");
+        $.ajax({
+          //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+          url: "api/messages",
+          //ルーティングで設定した通りhttpメソッドをgetに指定
+          type: 'get',
+          dataType: 'json',
+          //dataオプションでリクエストに値を含める
+          data: {id: last_message_id}
+        })
+        .done(function(messages) {
+          //追加するHTMLの入れ物を作る
+          var insertHTML = '';
+          //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+          $.each(messages, function(i,message) {
+            insertHTML += buildHTML(message)
+          });
+          //メッセージが入ったHTMLに、入れ物ごと追加
+          $(".main__chat_message").append(insertHTML);
+        })
+        .fail(function() {
+          alert('error');
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $(".main__chat_message").append(insertHTML);
-      })
-      .fail(function() {
-        alert('error');
-      });
-    }
+      }
     };
   setInterval(reloadMessages, 7000);
 });
